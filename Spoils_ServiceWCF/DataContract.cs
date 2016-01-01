bba0dcbb3e19fae1755b362d21179ee8dfc6923e
Guose <guose79@gmail.com>
2015-12-31 22:53:03 -0800

    Update WCF Service

---
+++ Spoils_ServiceWCF/DataContract.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spoils_ServiceWCF
+{
+    [DataContract]
+    public class DataContract
+    {
+        [DataMember]
+        public string CustomerName;
+
+        [DataMember]
+        public string JobNumber;
+
+        [DataMember]
+        public long FirstNumber;
+
+        [DataMember]
+        public long LastNumber;
+
+        [DataMember]
+        public int TextFileIndex;
+
+        [DataMember]
+        public List<string> TextFilesInDataFolder;
+
+        [DataMember]
+        public bool WasScanned;
+
+        [DataMember]
+        public DataTable RetrieveSpoilRecords;
+
+
+
+    }
+}
---
+++ Spoils_ServiceWCF/SpoilsWCFServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Spoils_ServiceWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "SpoilsWCFService" in both code and config file together.
+    public class SpoilsWCFServices : ISpoilsWCFServices
+    {
+        public DataContract CustomerName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoWork()
+        {
+        }
+    }
+}
